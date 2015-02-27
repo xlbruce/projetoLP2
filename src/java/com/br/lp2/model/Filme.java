d635858c61f59c53e75637d2ecc9fe520c51cfd5
31409695 <31409695@087998-P33S105.macklabs.br>
2015-02-27 09:16:36 -0300

    Foram adicionados novos atributos à classe "Filme"

--- src/java/com/br/lp2/model/Filme.java
+++ src/java/com/br/lp2/model/Filme.java
@@ -9,42 +9,168 @@ import java.io.Serializable;
 
 /**
  *
- * @author bruce
- * @version 1.0
+ * @author Gilson da Silva
+ * @version 1.1
  */
 public class Filme implements Serializable {
-    /* Deixei os atributos "diretor" e "distribuidora" pois não acho que valha
-     * a pena criar classes para tal, pois não é necessário guardar informações
-     * sobre esses campos.
-    */
-    private String titulo, diretor, distribuidora, situacao;
-    float duracao;
-    int censura;
     
+    private String titulo, situacao, idioma;
+    private ListaDeAtores listaAtores;
+    private Distribuidora distribuidora;
+    private Diretor diretor;
+    private int classificacao;
+    private int ano, pk, duracao;
+    private boolean legenda;
     
     // Esta enum representa as possiveis situações em que um filme pode se encontrar
-    private enum TiposSituacao {
+    public enum TiposSituacao {
         CARTAZ, ESTREIA, LANCAMENTO
-    }    
-
-    public Filme(String titulo, String diretor, String distribuidora, String situacao, float duracao, int censura) {
+    }
+    
+    public Filme(int pk, String titulo, String situacao, String idioma, ListaDeAtores listaAtores, Distribuidora distribuidora, Diretor diretor, int classificacao, int ano, int duracao, boolean legenda) {
         this.titulo = titulo;
-        this.diretor = diretor;
-        this.distribuidora = distribuidora;
         this.situacao = situacao;
+        this.idioma = idioma;
+        this.listaAtores = listaAtores;
+        this.distribuidora = distribuidora;
+        this.diretor = diretor;
+        this.classificacao = classificacao;
+        this.ano = ano;
+        this.pk = pk;
         this.duracao = duracao;
-        this.censura = censura;
+        this.legenda = legenda;
+    }  
+
+    //Getters e Setters
+    
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
+    /**
+     * @param situacao the situacao to set
+     */
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    /**
+     * @return the idioma
+     */
+    public String getIdioma() {
+        return idioma;
+    }
+
+    /**
+     * @param idioma the idioma to set
+     */
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    /**
+     * @return the listaAtores
+     */
+    public ListaDeAtores getListaAtores() {
+        return listaAtores;
+    }
+
+    /**
+     * @param listaAtores the listaAtores to set
+     */
+    public void setListaAtores(ListaDeAtores listaAtores) {
+        this.listaAtores = listaAtores;
+    }
+
+    /**
+     * @param distribuidora the distribuidora to set
+     */
+    public void setDistribuidora(Distribuidora distribuidora) {
+        this.distribuidora = distribuidora;
+    }
+
+    /**
+     * @param diretor the diretor to set
+     */
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    /**
+     * @return the classificacao
+     */
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    /**
+     * @param classificacao the classificacao to set
+     */
+    public void setClassificacao(int classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    /**
+     * @return the ano
+     */
+    public int getAno() {
+        return ano;
+    }
+
+    /**
+     * @param ano the ano to set
+     */
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    /**
+     * @return the pk
+     */
+    public int getPk() {
+        return pk;
+    }
+
+    /**
+     * @param pk the pk to set
+     */
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    /**
+     * @param duracao the duracao to set
+     */
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    /**
+     * @return the legenda
+     */
+    public boolean isLegenda() {
+        return legenda;
+    }
+
+    /**
+     * @param legenda the legenda to set
+     */
+    public void setLegenda(boolean legenda) {
+        this.legenda = legenda;
+    }
+    
     public String getTitulo() {
         return titulo;
     }
 
-    public String getDiretor() {
+    public Diretor getDiretor() {
         return diretor;
     }
 
-    public String getDistribuidora() {
+    public Distribuidora getDistribuidora() {
         return distribuidora;
     }
 
@@ -52,18 +178,21 @@ public class Filme implements Serializable {
         return situacao;
     }
 
-    public float getDuracao() {
+    public int getDuracao() {
         return duracao;
     }
 
     public int getCensura() {
-        return censura;
+        return getClassificacao();
     }
+    //-- Fim dos Getters e Setters
 
     @Override
     public String toString() {
-        return "Filme{" + "titulo=" + titulo + ", diretor=" + diretor + ", distribuidora=" + distribuidora + ", situacao=" + situacao + ", duracao=" + duracao + ", censura=" + censura + '}';
+        return "Filme{" + "titulo=" + titulo + ", situacao=" + situacao + ", idioma=" + idioma + ", listaAtores=" + listaAtores + ", distribuidora=" + distribuidora + ", diretor=" + diretor + ", classificacao=" + classificacao + ", ano=" + ano + ", pk=" + pk + ", duracao=" + duracao + ", legenda=" + legenda + '}';
     }
+
+    
     
     
     
