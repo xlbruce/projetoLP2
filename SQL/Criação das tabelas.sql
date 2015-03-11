e0cca0049bc1ab6f9f5a99082bdabc5311601ca9
xlbruce <gilsonsvieira@hotmail.com>
2015-03-11 17:19:11 -0300

    Script de criação das tabelas no banco de dados

---
+++ SQL/Criação das tabelas.sql
@@ -0,0 +1,25 @@
+create table cargo(id int not null primary key generated always as identity(start with 1, increment by 1), nome varchar(20));
+
+INSERT INTO cargo(NOME) VALUES('Gerente');
+INSERT INTO cargo(NOME) VALUES('Secretario');
+INSERT INTO cargo(NOME) VALUES('Atendente');
+INSERT INTO cargo(NOME) VALUES('Limpeza');
+
+CREATE TABLE departamento(id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), nome varchar(20));
+
+INSERT INTO departamento(nome) VALUES('Gerência');
+INSERT INTO departamento(nome) VALUES('Almoxarifado');
+INSERT INTO departamento(nome) VALUES('Secretaria');
+INSERT INTO departamento(nome) VALUES('RH');
+INSERT INTO departamento(nome) VALUES('Recepção');
+
+
+CREATE TABLE funcionario (id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),nome VARCHAR(20),rg VARCHAR(10),id_cargo INT, id_departamento INT);
+ALTER TABLE funcionario ADD FOREIGN KEY(id_cargo) REFERENCES cargo(id);
+ALTER TABLE funcionario ADD FOREIGN KEY(id_departamento) REFERENCES departamento(id);
+
+INSERT INTO FUNCIONARIO(nome,rg,id_cargo,id_departamento) VALUES ('Cacique','MG123456',1,1);
+INSERT INTO FUNCIONARIO(nome,rg,id_cargo,id_departamento) VALUES ('José','6543123',2,3);
+INSERT INTO FUNCIONARIO(nome,rg,id_cargo,id_departamento) VALUES ('Maria','8372643',3,5);
+INSERT INTO FUNCIONARIO(nome,rg,id_cargo,id_departamento) VALUES ('Sarah','6567567',3,5);
+INSERT INTO FUNCIONARIO(nome,rg,id_cargo,id_departamento) VALUES ('Otávio','123',4,2);
