02c74bfb808b9872afd3301450d87cd74f659f75
Gilson da Silva <gilsonsvieira@hotmail.com>
2015-03-11 09:07:08 -0300

    JavaBeans

---
+++ src/java/com/br/lp2/model/javabeans/Cargo.java
@@ -0,0 +1,31 @@
+
+package com.br.gilson.model.javabeans;
+
+/**
+ *
+ * @author 31409695
+ */
+public class Cargo {
+    private int id;
+    private String nome;
+
+    public Cargo(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargo{" + "id=" + id + ", nome=" + nome + '}';
+    }   
+    
+    
+}
---
+++ src/java/com/br/lp2/model/javabeans/Departamento.java
@@ -0,0 +1,31 @@
+
+package com.br.gilson.model.javabeans;
+
+/**
+ *
+ * @author 31409695
+ */
+public class Departamento {
+    private int id;
+    private String nome;
+
+    public Departamento(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" + "id=" + id + ", nome=" + nome + '}';
+    }
+    
+    
+}
