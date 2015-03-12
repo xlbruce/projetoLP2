45989a2e8204445de2d9c89cf51e6d80e326d3ba
Gilson da Silva <gilsonsvieira@hotmail.com>
2015-03-11 21:25:01 -0300

    Update DepartamentoDAOConcreto.java

--- src/java/com/br/lp2/model/DAO/DepartamentoDAOConcreto.java
+++ src/java/com/br/lp2/model/DAO/DepartamentoDAOConcreto.java
@@ -75,7 +75,7 @@ public class DepartamentoDAOconcreto implements DepartamentoDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return departamento;
     }
 
     @Override
