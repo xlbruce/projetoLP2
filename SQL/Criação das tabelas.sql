1531d00cc0eee8707a9473057b821afae925d177
31409695 <31409695@087874-P33S105.macklabs.br>
2015-03-12 10:15:47 -0300

    Comentários adicionados (criação das tabelas.sql)

--- SQL/Criação das tabelas.sql
+++ SQL/Criação das tabelas.sql
@@ -1,3 +1,4 @@
+-- Cria a tabela cargo e a inicializa com alguns valores
 create table cargo(id int not null primary key generated always as identity(start with 1, increment by 1), nome varchar(20));
 
 INSERT INTO cargo(NOME) VALUES('Gerente');
@@ -5,6 +6,7 @@ INSERT INTO cargo(NOME) VALUES('Secretario');
 INSERT INTO cargo(NOME) VALUES('Atendente');
 INSERT INTO cargo(NOME) VALUES('Limpeza');
 
+-- Cria a tabela departamento e a inicializa com alguns valores
 CREATE TABLE departamento(id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), nome varchar(20));
 
 INSERT INTO departamento(nome) VALUES('Gerência');
@@ -13,7 +15,7 @@ INSERT INTO departamento(nome) VALUES('Secretaria');
 INSERT INTO departamento(nome) VALUES('RH');
 INSERT INTO departamento(nome) VALUES('Recepção');
 
-
+-- Cria a tabela funcionario e a inicializa com alguns valores
 CREATE TABLE funcionario (id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),nome VARCHAR(20),rg VARCHAR(10),id_cargo INT, id_departamento INT);
 ALTER TABLE funcionario ADD FOREIGN KEY(id_cargo) REFERENCES cargo(id);
 ALTER TABLE funcionario ADD FOREIGN KEY(id_departamento) REFERENCES departamento(id);
