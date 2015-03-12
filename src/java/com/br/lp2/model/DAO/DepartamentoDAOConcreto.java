5b87f73736d3fa9e7ee176fd9b762d022aa4e8f2
AntonioNunes <antonio_florio@outlook.com>
2015-03-11 21:09:55 -0300

    Update DepartamentoDAOConcreto.java

--- src/java/com/br/lp2/model/DAO/DepartamentoDAOConcreto.java
+++ src/java/com/br/lp2/model/DAO/DepartamentoDAOConcreto.java
@@ -1,23 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.br.lp2.model.DAO;
 
 import com.br.lp2.model.ConnectionFactory.ConnectionFactory;
 import com.br.lp2.model.javabeans.Departamento;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author 31409695
  */
-public class DepartamentoDAOConcreto implements DepartamentoDAO {
-    
+public class DepartamentoDAOconcreto implements DepartamentoDAO {
+
     private Connection connection;
     private ResultSet rs;
-    private PreparedStatement ps;
-    
-    public DepartamentoDAOConcreto() {
+    private PreparedStatement pst;
+
+    public DepartamentoDAOconcreto() {
         ConnectionFactory cf = new ConnectionFactory();
         connection = cf.getConnection("derby");
     }
@@ -27,9 +36,9 @@ public class DepartamentoDAOConcreto implements DepartamentoDAO {
         boolean resultado = false;
         String sql = "INSERT INTO departamento (nome) VALUES (?)";
         try {
-            ps = connection.prepareStatement(sql);
-            ps.setString(1, d.getNome());
-            ps.execute();
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, d.getNome());
+            pst.execute();
             resultado = true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -42,9 +51,9 @@ public class DepartamentoDAOConcreto implements DepartamentoDAO {
         ArrayList<Departamento> departamentos = new ArrayList<>();
         String sql = "SELECT * FROM departamento";
         try {
-            ps = connection.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while(rs.next()) {
+            pst = connection.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()) {
                 departamentos.add(new Departamento(rs.getInt("id"), rs.getString("nome")));
             }
         } catch (Exception e) {
@@ -58,9 +67,9 @@ public class DepartamentoDAOConcreto implements DepartamentoDAO {
         Departamento departamento = null;
         String sql = "SELECT * FROM departamento WHERE id=?";
         try {
-            ps = connection.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while(rs.next()) {
+            pst = connection.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()) {
                 departamento = new Departamento(rs.getInt("id"), rs.getString("nome"));
             }
         } catch (Exception e) {
@@ -71,22 +80,79 @@ public class DepartamentoDAOConcreto implements DepartamentoDAO {
 
     @Override
     public Departamento readDepartamento(int codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Departamento departamento = null;
+
+        String sql = "SELECT * FROM departamento WHERE id=?";
+        try {
+            pst = connection.prepareStatement(sql);
+
+            pst.setInt(1, codigo);
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                departamento = new Departamento(rs.getInt("id"), rs.getString("nome"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DepartamentoDAOconcreto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return departamento;
     }
 
     @Override
     public boolean updateDepartamento(int codigo, Departamento d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        boolean resultado = false;
+        try {
+            String sql = "UPDATE departamento SET nome=? WHERE id=?";
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, d.getNome());
+            pst.setInt(2, codigo);
+
+            int r = pst.executeUpdate();
+
+            if (r > 0) {
+                resultado = true;
+            } else {
+                resultado = false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DepartamentoDAOconcreto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
     }
 
     @Override
     public boolean deleteDepartamento(int codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+            boolean resultado = false;
+           try {
+               String sql = "DELETE * FROM departamento WHERE id=?";
+            
+            pst = connection.prepareStatement(sql);
+            
+            pst.setInt(1, codigo);
+            resultado = pst.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DepartamentoDAOconcreto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
     }
 
     @Override
     public boolean deleteDepartamento(Departamento d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean resultado = false;
+        try {
+            String sql = "DELETE * FROM departamento WHERE id=?";
+            
+            pst = connection.prepareStatement(sql);
+            
+            pst.setInt(1, d.getId());
+            
+            resultado = pst.execute();
+        } catch (SQLException sQLException) {
+        }
+        return resultado;
     }
-    
+
 }
